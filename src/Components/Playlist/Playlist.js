2a15b40e8c29fde0398166ea2b4fcf27eabef9e0
Nick Paduchowski <82173102+nick-paduchowski@users.noreply.github.com>
2022-01-02 16:44:33 -0500

    Fixed playlist track functionality

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -21,7 +21,7 @@ class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input defaultValue="New Playlist" onChange={this.handleNameChange} />
-        <TrackList playlistTracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
+        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
         <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
